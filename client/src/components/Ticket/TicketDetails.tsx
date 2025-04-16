c07020aa8e4946bc62ab4f67e3f7a39553b8d60e
Sybatron <vipsister2014@gmail.com>
2025-04-16 16:42:04 +0300

    add profile types and zod schemas

---
+++ client/src/components/Ticket/TicketDetails.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
+import { BellRing, Check } from "lucide-react";
+
+const MockupApi = () => {
+	return new Promise();
+};
+
+export const TicketDetails = (id: string) => {
+	const { data } = useQuery({ queryKey: ["tickets", id] });
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Notifications</CardTitle>
+				<CardDescription>You have 3 unread messages.</CardDescription>
+			</CardHeader>
+			<CardContent className="grid gap-4">
+				<div className="flex items-center space-x-4 rounded-md border p-4">
+					<BellRing />
+					<div className="flex-1 space-y-1">
+						<p className="text-sm leading-none font-medium">
+							Push Notifications
+						</p>
+						<p className="text-muted-foreground text-sm">
+							Send notifications to device.
+						</p>
+					</div>
+				</div>
+			</CardContent>
+			<CardFooter>
+				<Button className="w-full">
+					<Check /> Mark all as read
+				</Button>
+			</CardFooter>
+		</Card>
+	);
+};
