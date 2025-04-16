6f22683800cb60e4535d3206bc7c964b62862f92
Sybatron <vipsister2014@gmail.com>
2025-04-16 15:51:52 +0300

    add Ticket type

---
+++ client/src/components/Ticket/types/Ticket.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const TicketSchema = z.object({
+	title: z.string().min(3),
+	description: z.string().min(10),
+});
+
+export type Ticket = z.infer<typeof TicketSchema>;
