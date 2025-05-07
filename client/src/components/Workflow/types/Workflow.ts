14f3e4915683bd5ece4ec8bf58f5f91fccb95abb
Sybatron <vipsister2014@gmail.com>
2025-05-07 16:21:46 +0300

    add workflow schema

---
+++ client/src/components/Workflow/types/Workflow.ts
@@ -0,0 +1,12 @@
+import { ProjectSchema } from "@/components/Project/types/Project";
+import { TicketSchema } from "@/components/Ticket/types/Ticket";
+import { z } from "zod";
+
+export const WorkflowSchema = z.object({
+	id: z.coerce.string().optional(),
+	project: ProjectSchema.shape.id,
+	fromStatus: TicketSchema.shape.status,
+	toStatus: TicketSchema.shape.status,
+});
+
+export type Workflow = z.infer<typeof WorkflowSchema>;
