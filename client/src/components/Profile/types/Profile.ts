3e64a92a851f76e57b69531deec659e37a22e4a7
Sybatron <vipsister2014@gmail.com>
2025-05-07 10:08:25 +0300

    update profile type

--- client/src/components/Profile/types/Profile.ts
+++ client/src/components/Profile/types/Profile.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-	id: z.string().optional(),
+	id: z.coerce.string().optional(),
 	fname: z.string().min(3),
 	lname: z.string().min(3),
 	username: z.string().min(3),
 	password: z.string().min(8).max(32),
+	createdAt: z.date().min(new Date("1900-01-01"), { message: "Too old" }),
+	email: z.string().email().max(100),
+	role: z.string().min(3).max(20),
 });
 export type User = z.infer<typeof UserSchema>;
 
@@ -14,5 +17,6 @@ export const ProfileSchema = UserSchema.pick({
 	fname: true,
 	lname: true,
 	username: true,
+	// role: true,
 });
 export type Profile = z.infer<typeof ProfileSchema>;
