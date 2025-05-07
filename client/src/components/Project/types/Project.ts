db980a38183b81790be30cb10ffab40229da22ab
Sybatron <vipsister2014@gmail.com>
2025-05-07 10:00:45 +0300

    update Project type

--- client/src/components/Project/types/Project.ts
+++ client/src/components/Project/types/Project.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
 export const ProjectSchema = z.object({
-	id: z.string().optional(),
-	name: z.string().min(3),
+	id: z.coerce.string().optional(),
+	name: z.string().min(3).max(100),
+	description: z.string().min(10),
+	createdAt: z.date().min(new Date("1900-01-01"), { message: "Too old" }),
 });
 
-export type Project = z.infer<typeof ProjectSchema>;
+const ProjectWithoutCA = ProjectSchema.omit({ createdAt: true });
+
+export type Project = z.infer<typeof ProjectWithoutCA>;
