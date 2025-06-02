c2953be43b69b6450b52985195667adc5f17eb37
pety02 <pety02@abv.bg>
2025-06-02 23:32:24 +0300

    FilterRoute ts file created and multi-select tsx file updated.

---
+++ client/src/components/TicketFilter/FilterRoute.ts
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+type TicketStatusSearchOptions = 'not-started' | 'in-progress' | 'completed'
+
+type TicketSearch = {
+    page: number;
+    filter: string;
+    sort: TicketStatusSearchOptions;
+};
+
+// export const Route = createFileRoute('/tickets')
