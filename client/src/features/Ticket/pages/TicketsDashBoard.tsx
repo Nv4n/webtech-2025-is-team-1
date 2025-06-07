166b0351db40e83febb58f98e38730602ce48848
Sybatron <vipsister2014@gmail.com>
2025-06-07 23:39:10 +0300

    updates

--- client/src/features/Ticket/pages/TicketsDashBoard.tsx
+++ client/src/features/Ticket/pages/TicketsDashBoard.tsx
@@ -1,5 +1,6 @@
 import { TicketsFilter } from "@/features/Ticket/components/TicketFilter";
 import { TicketsGroup } from "@/features/Ticket/components/TicketsGroup";
+import { TicketStatuses } from "@/features/Ticket/types/Ticket";
 
 export function TicketsDashboard() {
 	return (
@@ -19,9 +20,9 @@ export function TicketsDashboardWithFilter() {
 		<div>
 			<TicketsFilter />
 			<div className="flex w-full justify-center space-x-4 p-4">
-				<TicketsGroup status="not-started" />
-				<TicketsGroup status="in-progress" />
-				<TicketsGroup status="completed" />
+				{TicketStatuses.map((status) => {
+					return <TicketsGroup status={status} />;
+				})}
 			</div>
 		</div>
 	);
