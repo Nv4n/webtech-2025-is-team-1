901b765fda4014d45877e79e6e58fb2caeb644c0
Sybatron <vipsister2014@gmail.com>
2025-06-09 00:30:05 +0300

    updated filter

--- client/src/features/Ticket/types/TicketFilter.ts
+++ client/src/features/Ticket/types/TicketFilter.ts
@@ -8,6 +8,12 @@
 import { TicketStatuses } from "@/features/Ticket/types/Ticket";
 import { z } from "zod";
 
+const optionSchema = z.object({
+	label: z.string(),
+	value: z.string(),
+	disable: z.boolean().optional(),
+});
+
 export const TicketFilterSchema = z.object({
 	projectIds: z.array(z.number()).optional(),
 	assigneeIds: z.array(z.number()).optional(),
@@ -15,3 +21,34 @@ export const TicketFilterSchema = z.object({
 });
 
 export type TicketFilter = z.infer<typeof TicketFilterSchema>;
+
+export const TicketFilterFormSchema = z.object({
+	projectIds: z.array(optionSchema),
+	assigneeIds: z.array(optionSchema),
+	statuses: z.array(optionSchema),
+});
+
+export type TicketFilterForm = z.infer<typeof TicketFilterFormSchema>;
+
+export const TicketTransformSchema = z.object({
+	statuses: z
+		.array(optionSchema)
+		.transform((opts) => {
+			console.log(opts);
+
+			return opts.map((opt) => opt.value);
+		})
+		.optional(),
+	assigneeIds: z
+		.array(optionSchema)
+		.transform((opts) => {
+			return opts.map((opt) => Number(opt.value));
+		})
+		.optional(),
+	projectIds: z
+		.array(optionSchema)
+		.transform((opts) => {
+			return opts.map((opt) => opt.value);
+		})
+		.optional(),
+});
