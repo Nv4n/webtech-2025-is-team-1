d2d790fb875de22253d3d3f84371d213f195fbeb
Sybatron <vipsister2014@gmail.com>
2025-06-08 17:06:44 +0300

    update shared components

--- client/src/components/CardBadge.tsx
+++ client/src/components/CardBadge.tsx
@@ -3,13 +3,18 @@ import { Badge } from "@/components/ui/badge";
 type CardBadgeProps = {
 	content: string;
 };
+const statusBadgeStyles = {
+	Open: "bg-blue-500 text-white dark:text-white",
+	InProgress: "bg-amber-400 text-white dark:text-gray-900",
+	InReview: "bg-purple-500 text-white dark:text-white",
+	Testing: "bg-indigo-400 text-white dark:text-white",
+	Done: "bg-green-600 text-white dark:text-white",
+	Closed: "bg-gray-700 text-white dark:text-white",
+	Blocked: "bg-red-600 text-white dark:text-white",
+	Cancelled: "bg-zinc-400 text-white dark:text-gray-900",
+};
 
 export const CardBadge = ({ content }: CardBadgeProps) => {
-	const statusBadgeStyles = {
-		"not-started": "bg-red-700 text-white dark:text-white",
-		"in-progress": "bg-amber-300 text-gray-900 dark:text-gray-900",
-		completed: "bg-green-900 text-white dark:text-white",
-	};
 	return (
 		<Badge
 			variant="default"
